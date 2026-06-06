5b99930779f6981ab8f0ee4f3bec407ec1f47da8
zpcode <zpkemem@gmail.com>
2026-06-06 14:46:19 +0800

    Update bard-ai.py

--- plugins/bard-ai.py
+++ plugins/bard-ai.py
@@ -26,7 +26,7 @@ async def chat_gpt(client, message):
         else:
             prs = await message.reply_text(f"<emoji id=5319230516929502602>🔍</emoji>proccesing....")
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://api.betabotz.eu.org/api/search/bard-ai?text={a}&apikey=Btz-VnkOr')
+            response = requests.get(f'https://api.botcahx.eu.org/api/search/bard-ai?text={a}&apikey=4LuZk4hH')
 
             try:
                 if "message" in response.json():
